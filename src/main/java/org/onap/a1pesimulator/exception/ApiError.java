e0d5c84589e44299924f9183632d3becfffad7b5
Stanislav Marszalek <s.marszalek2@partner.samsung.com>
2021-08-06 11:12:26 +0200

    O1 PM Bulk support - Unknown cell handling
    Issue-ID: INT-1945
    
    Signed-off-by: Stanislav Marszalek <s.marszalek2@partner.samsung.com>
    Change-Id: I02d3684c98d563d7f386de2fdf032e930ac12b3f

---
+++ src/main/java/org/onap/a1pesimulator/exception/ApiError.java
@@ -0,0 +1,30 @@
+package org.onap.a1pesimulator.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+class ApiError {
+
+    private HttpStatus status;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private LocalDateTime timestamp;
+    private String message;
+    private String debugMessage;
+
+    private ApiError() {
+        timestamp = LocalDateTime.now();
+    }
+
+    ApiError(HttpStatus status) {
+        this();
+        this.status = status;
+    }
+}
