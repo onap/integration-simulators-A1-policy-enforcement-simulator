f176c991d7ea04b10503ef79c98c9adc87e91fb6
Stanislav Marszalek <s.marszalek2@partner.samsung.com>
2021-08-26 08:28:33 +0200

    Adding copyright to A1 PE Simulator new files
    Issue-ID: INT-1963
    
    Signed-off-by: Stanislav Marszalek <s.marszalek2@partner.samsung.com>
    Change-Id: I02d3684c98d563d7f386de2fdf032e935ac47b3f

--- src/main/java/org/onap/a1pesimulator/service/fileready/PMBulkFileService.java
+++ src/main/java/org/onap/a1pesimulator/service/fileready/PMBulkFileService.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (C) 2021 Samsung Electronics
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
 package org.onap.a1pesimulator.service.fileready;
 
 import static java.util.Comparator.comparing;
@@ -205,12 +218,12 @@ public class PMBulkFileService {
             event.getMeasurementFields().getAdditionalMeasurements().stream()
                     .filter(additionalMeasurement -> measurmentMap.containsKey(additionalMeasurement.getName())).forEach(additionalMeasurement -> {
 
-                //r elements
-                Element r = doc.createElement("r");
-                measValue.appendChild(r);
-                r.setAttribute("p", measurmentMap.get(additionalMeasurement.getName()));
-                r.setTextContent(additionalMeasurement.getHashMap().get(MEASUREMENT_FIELD_VALUE));
-            });
+                        //r elements
+                        Element r = doc.createElement("r");
+                        measValue.appendChild(r);
+                        r.setAttribute("p", measurmentMap.get(additionalMeasurement.getName()));
+                        r.setTextContent(additionalMeasurement.getHashMap().get(MEASUREMENT_FIELD_VALUE));
+                    });
         });
     }
 
